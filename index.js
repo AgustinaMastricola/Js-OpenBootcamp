2843e0d36bf954fecbd8fc44a3ffb86cba36c7dd
Agustina Mastricola <agus.ppye@gmail.com>
2023-01-02 14:35:26 -0300

    Second module exercise

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hola");
+/*
+Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+- Tu nombre (string)
+- Tu edad (number)
+- ¿Eres desarrollador? (boolean)
+- Tu fecha de nacimiento (Date)
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+*/
+
+let myName = "Agustina";
+let myAge = "27";
+let isDeveloper = true;
+let myDate = new Date(1995, 5, 6);
+const favoriteBook = {
+    name: "La casa de los Espiritus",
+    author: "Isabel Allende",
+    pages: 418,
+    year: 1982,
+    editorial: "Plaza y Janes"
+}
